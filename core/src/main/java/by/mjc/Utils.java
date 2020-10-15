849eb11c76741be3ed1c1477833af023d00d12f4
Nastya <rodnovanastya@gmail.com>
2020-10-15 20:08:38 +0300

    initial commit

---
+++ core/src/main/java/by/mjc/Utils.java
@@ -0,0 +1,14 @@
+package by.mjc;
+
+import com.epam.utils.StringUtils;
+
+public class Utils {
+    public static boolean isAllPositiveNumbers(String... str) {
+        for (String s : str) {
+            if (!StringUtils.isPositiveNumber(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
